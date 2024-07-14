4e81cc87563177f4e13b8bf81dae54f26987cdcf
Jose Augusto Rodriguez <jose.augus.1996@gmail.com>
2024-07-14 18:38:09 -0300

    add funcionalidad login.js

--- js/login.js
+++ js/login.js
@@ -17,6 +17,12 @@ document.querySelector(".dropdown-item.cardUserSalir").addEventListener("click",
     login.classList.toggle("hiddenUser");
 })
 
+// document.querySelector(".linkRegistrarme").addEventListener("click",function(){
+//     const formulario= document.querySelector(".formulario-login");
+// })
+
+
+
 /**/
 // Proceso de validacion del login
 async function AperturaData(email, password){
@@ -80,4 +86,53 @@ form.addEventListener("submit",function(event){
     password.value="";
 })
 
-// mostrar card user
+// cambiar el contenido del form
+function changeForm(val){
+    const formulario=document.querySelector("#signIn-signUp");
+
+    if(val==1){
+        formulario.innerHTML=`
+        <h2>Registrarse</h2>
+        <div class="form-floating mb-3">
+          <input type="text" class="form-control email-form" placeholder="name@example.com" required>
+          <label for="floatingInput">Nombre</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input type="text" class="form-control email-form" placeholder="name@example.com" required>
+          <label for="floatingInput">Apellido</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input type="email" class="form-control email-form" id="floatingInput" placeholder="name@example.com" required>
+          <label for="floatingInput">Email</label>
+        </div>
+        <div class="form-floating">
+          <input type="password" class="form-control password-form" id="floatingPassword" placeholder="Password" required>
+          <label for="floatingPassword">Contraseña</label>
+        </div>
+        <div class="btn-group" role="group" aria-label="Basic example">
+          <button type="submit" class="btn btn-primary">Registrarme</button>
+          <button type="reset" class="btn btn-primary">Limpiar</button>
+        </div>
+        <span class="linkRegistrarme"><a onclick="changeForm(0)" href="#">&#8592; Ingresar</a></span>
+        `;
+    }
+
+    if(val==0){
+        formulario.innerHTML=`
+        <h2>Iniciar Sesion</h2>
+        <div class="form-floating mb-3">
+          <input type="email" class="form-control email-form" id="floatingInput" placeholder="name@example.com" required>
+          <label for="floatingInput">Email</label>
+        </div>
+        <div class="form-floating">
+          <input type="password" class="form-control password-form" id="floatingPassword" placeholder="Password" required>
+          <label for="floatingPassword">Password</label>
+        </div>
+        <div class="btn-group" role="group" aria-label="Basic example">
+          <button type="submit" class="btn btn-primary">Ingresar</button>
+          <button type="reset" class="btn btn-primary">Limpiar</button>
+        </div>
+        <span class="linkRegistrarme"><a onclick="changeForm(1)" href="#">Registrarme &#8594;</a></span>
+        `
+    }
+}
